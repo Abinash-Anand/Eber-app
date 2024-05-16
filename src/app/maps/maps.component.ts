787ec415005fc518603f67483f59cf904123f1fb
Abinash Anand <abinashanandab@gmail.com>
2024-05-17 00:51:43 +0530

    userlocation added

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -2,21 +2,33 @@ import { AfterViewChecked, AfterViewInit, Component, ElementRef, OnInit, ViewChi
 import { environment } from '../../environment';
 import { Loader } from '@googlemaps/js-api-loader';
 import { MapService } from '../services/maps/mapsApi.service';
+import { response } from 'express';
 
 @Component({
   selector: 'app-maps',
   templateUrl: './maps.component.html',
   styleUrl: './maps.component.css'
 })
-export class MapsComponent implements AfterViewInit {
+export class MapsComponent implements OnInit, AfterViewInit {
   @ViewChild('mapRef') mapElement: ElementRef
   
   constructor(private mapService: MapService) {
     
   }
+  ngOnInit(): void {
+   this.getUserLocation()
+  }
   ngAfterViewInit() {
     console.log(this.mapElement.nativeElement);
     
    this.mapService.googleMapsApi(this.mapElement.nativeElement)
+  }
+
+  getUserLocation() {
+     this.mapService.getLocationService().then(response => {
+      
+       console.log(response.lng, response.lat);
+        
+      })
   }
 }
--- src/app/services/maps/mapsApi.service.ts
+++ src/app/services/maps/mapsApi.service.ts
@@ -1,37 +1,56 @@
-  import { ElementRef, Injectable } from '@angular/core';
-  import { Loader } from '@googlemaps/js-api-loader';
-  import { environment } from '../../../environment';
+import { ElementRef, Injectable } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
+import { environment } from '../../../environment';
 
-  @Injectable({
-    providedIn: 'root'
-  })
-  export class MapService {
+@Injectable({
+  providedIn: 'root'
+})
+export class MapService {
 
-    
-    constructor() { }
+  userGeolocation: { lat: number, lng: number };
 
-    googleMapsApi(mapElement) {
-      const loader = new Loader({
-        apiKey: environment.googleMapsApiKey,
-        version: "weekly", // Change this to the version you need
-        libraries: ["places"] // Add any additional libraries you need
+  constructor() { }
+
+  
+  //===========================Geolocation api Service=============================
+  getLocationService(): Promise<any>{
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(response => {
+        this.userGeolocation = { lat: response.coords.latitude, lng: response.coords.longitude };
+        resolve(this.userGeolocation);
+      }, error => {
+        reject("Error getting geolocation: " + error.message);
       });
+    });
+  } 
+  //=================================MAP Service===================================================
+  googleMapsApi(mapElement) {
+    const loader = new Loader({
+      apiKey: environment.googleMapsApiKey,
+      version: "weekly", // Change this to the version you need
+      libraries: ["places",] // Add any additional libraries you need
+    });
 
-      loader.load().then(() => {
-        // Google Maps API loaded successfully
-        this.initializeMap(mapElement);
-      }).catch((err) => {
-        console.error('Error loading Google Maps API:', err);
-      });
-    }
-
-    initializeMap(mapElement: HTMLElement) {
-      // Initialize your map here
-      const map = new google.maps.Map(mapElement, {
-        center: { lat: 18.5204, lng: 73.8567 },
-        zoom: 12
-      });
-    }
-
-
+    loader.load().then(() => {
+      // Google Maps API loaded successfully
+      this.initializeMap(mapElement, this.userGeolocation);
+    }).catch((err) => {
+      console.error('Error loading Google Maps API:', err);
+    });
   }
+
+  initializeMap(mapElement: HTMLElement, userGeolocation: { lat: number, lng: number }) {
+    // Initialize your map here
+    const map = new google.maps.Map(mapElement, {
+      center: { lat: userGeolocation.lat, lng: userGeolocation.lng },
+      zoom: 18
+    });
+
+    // Add marker for user's location
+    const userMarker = new google.maps.Marker({
+      position: { lat: userGeolocation.lat, lng: userGeolocation.lng },
+      map: map,
+      title: 'Your Location'
+    });
+  }
+}
