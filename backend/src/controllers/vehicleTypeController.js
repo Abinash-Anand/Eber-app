38144d8221853bcf918cd633e09129402f760653
Abinash Anand <abinashanandab@gmail.com>
2024-09-20 02:17:43 +0530

    pricing and vehicletype related

--- backend/src/controllers/vehicleTypeController.js
+++ backend/src/controllers/vehicleTypeController.js
@@ -1,6 +1,6 @@
 const { vehicleTypeModel } = require('../models/modelPackage'); // Ensure this path is correct
 const fs = require('fs'); // Import the File System module for file operations
-
+const Pricing = require('../models/pricingModel')
 const vehicleTypeController = async (req, res) => {
   
   try {
@@ -52,16 +52,29 @@ const updateVehicleData = async (req, res) => {
   try {
     const vehicleId = req.params.id; // Get the vehicle ID from the request parameters
     let updateFields = {}; // Initialize an object to hold update fields
-
+    
     // Fetch the current vehicle data to retrieve the old image path
     const vehicle = await vehicleTypeModel.findById(vehicleId);
+
+    console.log("vehicle: ",vehicle.vehicleType)
+    // const firstLetterUpperCase =  vehicle.vehicletype.charAt(0).toUpperCase() + vehicle.vehicletype.slice(1)  
+
+    const pricingUpdate =  await Pricing.findOne({});
     if (!vehicle) {
       return res.status(404).send('Vehicle not found'); // Check if the vehicle exists
     }
-
-    // Update the vehicle type if provided in the request body
+    console.log("Pricing: ", pricingUpdate)
+    if (!pricingUpdate) {
+      console.error("Pricing model do not exists for this vehicleType")
+    } 
+      pricingUpdate.vehicleType = req.body.type;
+     await pricingUpdate.save();
+      console.log("updated Pricing: ", pricingUpdate)
+     // Update the vehicle type if provided in the request body
     if (req.body.type) {
-      updateFields.vehicleType = req.body.type; // Set the new vehicle type
+       console.log("Update type: ", req.body.type)
+       updateFields.vehicleType = req.body.type; // Set the new vehicle type
+      
     }
 
     // Handle the vehicle image upload, if provided
@@ -90,11 +103,10 @@ const updateVehicleData = async (req, res) => {
 
     // Update the vehicle document in the database
     const updatedVehicle = await vehicleTypeModel.findByIdAndUpdate(vehicleId, updateFields, { new: true });
-
+  
     if (!updatedVehicle) {
       return res.status(404).send('Vehicle not found'); // Handle case where vehicle update fails
     }
-
     // Return the updated vehicle data with a success message
     res.status(200).json({ message: 'Vehicle updated successfully', vehicle: updatedVehicle });
 
