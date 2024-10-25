f270ae83a9872aee020c4471194a5e97d97b3128
Abinash Anand <abinashanandab@gmail.com>
2024-10-25 23:54:28 +0530

    production server Port integrated

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 require('dotenv')
-mongoose.connect('mongodb://localhost:27017/')
+mongoose.connect('mongodb://localhost:27017/eberDatabase')
 const db = mongoose.connect
 console.log("Database Connection Established📚, connected to EberDatabase", db);
--- src/environment.ts
+++ src/environment.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
   googleMapsApiKey: 'AIzaSyAgqVWjf2oAALcroMzNhjLyuIzgdFsmb0Y',
   countryApiUrl: 'https://restcountries.com/v3.1',
-  backendServerPORT: 'http://localhost:5000',
+  backendServerPORT: 'https://eber-server.onrender.com',
   stripePublishableKey:'pk_test_51QB8m5RtOBu9UDCDGUSdlIrEYiKpUH2ZEn9an0ig0hRWCYebxHVle4nsPoIs2XhLTL8rQzV1ISMpqeuPCdYtKbQt00LNZa4kVU',
-  socketUrl: 'http://localhost:5000',
+  socketUrl: 'https://eber-server.onrender.com',
   vapidPublicKey: "BLcQN_PtWOW32h56KkU50Fcmg8nMlCmZBKP2FkvMF9HH8s-kjmFbWkYWPcERK9XeygZwf5fmvWB3XUNEWcGmFXQ",
   // vapidPrivateKey: 'hDmEqUhxnvZF9iMqMNx1lZ8M8_HcJJzH8Hy-baWG8Sc'
   // webPushPublicKey: 'BCewtPamBMBtx-bgB51Rlzki9p75Rt1Gq6JQnrXq88i4hRSaNK2W7whTThB9iSsDaaQIHYba2xfJk2Moa7sGpC0',
